35648b776d42ce35a5299ecd7ce885e799a1c101
Adam <apacz_0@o2.pl>
2018-12-19 14:18:32 +0100

    refactor

--- src/main/java/com/tass/Main.java
+++ src/main/java/com/tass/Main.java
@@ -1,72 +1,25 @@
 package com.tass;
 
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.tass.api.Plane;
-import com.tass.api.graph.Tuple;
+import com.tass.api.Airport;
 import com.tass.api.graph.Value;
 import com.tass.service.GraphService;
-import com.tass.service.JsonService;
 import com.tass.service.WikiService;
 import edu.uci.ics.jung.graph.DirectedGraph;
-import edu.uci.ics.jung.graph.DirectedSparseGraph;
-import edu.uci.ics.jung.graph.util.Pair;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Main {
 
     private static final boolean GENERATE_FROM_SCRATCH = false;
 
     public static void main(String[] args) {
-        JsonService jsonService = JsonService.getInstance();
         GraphService graphService = GraphService.getInstance();
         WikiService wikiService = WikiService.getInstance();
-        ObjectMapper objectMapper = new ObjectMapper();
-        DirectedGraph<String, Value> graph;
 
-//        String from = "20181001";
-//        String to = "20181101";
-//        Airport airport = wikiService.getAirportViews("WMI", from, to);
-//        System.out.println(airport.getName() + ": " + airport.getViews());
+        DirectedGraph<String, Value> graph = graphService.generateGraph(GENERATE_FROM_SCRATCH);
 
-
-        if(GENERATE_FROM_SCRATCH){
-            Set<Plane> planes = jsonService.getPlanesFromFiles();
-            graph = graphService.generateGraph(planes);
-
-            Set<Tuple> set = new HashSet<>();
-
-            for (Value value : graph.getEdges()) {
-                Pair<String> endpoints = graph.getEndpoints(value);
-                Tuple tuple = new Tuple(endpoints.getFirst(), endpoints.getSecond(), value);
-                set.add(tuple);
-            }
-
-            try {
-                objectMapper.writeValue(new File("graph.json"), set);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        else {
-            Set<Tuple> set = new HashSet<>();
-            try {
-                set = objectMapper.readValue(new File("graph.json"), new TypeReference<Set<Tuple>>(){});
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            graph = new DirectedSparseGraph<>();
-
-            for (Tuple tuple : set) {
-                graph.addEdge(tuple.getValue(), tuple.getFrom(), tuple.getTo());
-            }
-        }
+        String from = "20181001";
+        String to = "20181101";
+        Airport airport = wikiService.getAirportViews("WMI", from, to);
+        System.out.println(airport.getName() + ": " + airport.getViews());
     }
 }
--- src/main/java/com/tass/service/GraphService.java
+++ src/main/java/com/tass/service/GraphService.java
@@ -1,11 +1,18 @@
 package com.tass.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tass.api.Plane;
+import com.tass.api.graph.Tuple;
 import com.tass.api.graph.Value;
 import com.tass.utils.PlaneTypeEnum;
 import edu.uci.ics.jung.graph.DirectedGraph;
 import edu.uci.ics.jung.graph.DirectedSparseGraph;
+import edu.uci.ics.jung.graph.util.Pair;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -13,6 +20,11 @@ import java.util.stream.Collectors;
 
 public class GraphService {
     private static GraphService graphService;
+    private JsonService jsonService;
+
+    public GraphService() {
+        jsonService = JsonService.getInstance();
+    }
 
     public static GraphService getInstance(){
         if(graphService == null){
@@ -21,7 +33,7 @@ public class GraphService {
         return graphService;
     }
 
-    public DirectedGraph<String, Value> generateGraph(Set<Plane> planes){
+    private DirectedGraph<String, Value> generateGraph(Set<Plane> planes){
         DirectedGraph<String, Value> graph = new DirectedSparseGraph<>();
 
         List<Plane> copyOfPlanes = new ArrayList<>(planes);
@@ -43,6 +55,50 @@ public class GraphService {
             graph.addEdge(new Value(value), from, to);
 
             copyOfPlanes.removeAll(planesToCountValue);
+        }
+
+        return graph;
+    }
+
+    public DirectedGraph<String, Value> generateGraph(boolean generateFromScratch){
+        ObjectMapper objectMapper = new ObjectMapper();
+        DirectedGraph<String, Value> graph;
+
+        if(generateFromScratch){
+            Set<Plane> planes = jsonService.getPlanesFromFiles();
+            graph = generateGraph(planes);
+
+            Set<Tuple> set = new HashSet<>();
+
+            for (Value value : graph.getEdges()) {
+                Pair<String> endpoints = graph.getEndpoints(value);
+                Tuple tuple = new Tuple(endpoints.getFirst(), endpoints.getSecond(), value);
+                set.add(tuple);
+            }
+
+            try {
+                objectMapper.writeValue(new File("graph.json"), set);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            Set<Tuple> set = new HashSet<>();
+            try {
+                set = objectMapper.readValue(new File("graph.json"), new TypeReference<Set<Tuple>>(){});
+            }
+            catch (FileNotFoundException e) {
+                throw new RuntimeException("JSON file with graph cannot be found");
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            graph = new DirectedSparseGraph<>();
+
+            for (Tuple tuple : set) {
+                graph.addEdge(tuple.getValue(), tuple.getFrom(), tuple.getTo());
+            }
         }
 
         return graph;
