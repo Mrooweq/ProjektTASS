7a3619c68192d8b85eae51a13bf0de3dbcc873bc
Wojciech Sobczyk <sobczyk-wojciech@wp.pl>
2018-12-22 17:32:16 +0100

    Only AirportViewService is invoked from Main

--- src/main/java/com/tass/Main.java
+++ src/main/java/com/tass/Main.java
@@ -1,25 +1,15 @@
 package com.tass;
 
-
-import com.tass.api.Airport;
-import com.tass.api.graph.Value;
-import com.tass.service.GraphService;
-import com.tass.service.WikiService;
-import edu.uci.ics.jung.graph.DirectedGraph;
+import com.tass.service.AirportViewsService;
+import java.util.Map;
 
 public class Main {
-
-    private static final boolean GENERATE_FROM_SCRATCH = false;
-
     public static void main(String[] args) {
-        GraphService graphService = GraphService.getInstance();
-        WikiService wikiService = WikiService.getInstance();
-
-        DirectedGraph<String, Value> graph = graphService.generateGraph(GENERATE_FROM_SCRATCH);
+        AirportViewsService airportViewsService = AirportViewsService.getInstance();
 
         String from = "20181001";
         String to = "20181101";
-        Airport airport = wikiService.getAirportViews("WMI", from, to);
-        System.out.println(airport.getName() + ": " + airport.getViews());
+        Map<String,Long> airportViews = airportViewsService.getAirportViews(from, to);
+        airportViewsService.print(airportViews);
     }
 }
