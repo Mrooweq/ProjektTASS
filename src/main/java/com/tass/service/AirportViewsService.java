87468c3058dfb77a68c09710a7c5cd2047d6523c
Wojciech Sobczyk <sobczyk-wojciech@wp.pl>
2018-12-22 15:14:31 +0100

    Added method which return AirportViewsMap

--- src/main/java/com/tass/service/AirportViewsService.java
+++ src/main/java/com/tass/service/AirportViewsService.java
@@ -1,11 +1,41 @@
 package com.tass.service;
 
+import com.tass.api.graph.Value;
+import edu.uci.ics.jung.graph.DirectedGraph;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class AirportViewsService {
     private static AirportViewsService airportViewsService;
+
+    private static final boolean GENERATE_FROM_SCRATCH = false;
+
+    private GraphService graphService;
+    private WikiService wikiService;
+
+    public AirportViewsService() {
+        graphService = GraphService.getInstance();
+        wikiService = WikiService.getInstance();
+    }
 
     public static AirportViewsService getInstance () {
         if (airportViewsService == null)
             airportViewsService = new AirportViewsService();
         return airportViewsService;
     }
-}
+
+    public Map<String, Long> getAirportViews (DirectedGraph directedGraph, String from, String to) {
+        Map<String, Long> airportViews = new HashMap<>();
+        DirectedGraph<String, Value> graph = graphService.generateGraph(GENERATE_FROM_SCRATCH);
+        Collection<String> airports = graph.getVertices();
+
+        for (String airport : airports) {
+            Long views = wikiService.getAirportViews(airport, from, to);
+            airportViews.put(airport, views);
+        }
+
+        return airportViews;
+    }
+ }
